7e6dd166ef9723c7a0ce7c9a1839ffe7993e6125
monomadic <email@server.com>
2022-01-31 18:34:17 +0800

    added tasks: token, account

---
+++ tasks/accountTasks.js
@@ -0,0 +1,17 @@
+import { task } from 'hardhat/config';
+
+task("accounts", "Prints the list of accounts", async(_taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        const [address, balance] = await Promise.all([
+            account.getAddress(),
+            account.getBalance(),
+        ]);
+
+        const displayBalance = Number(
+            hre.ethers.utils.formatUnits(balance, 18)
+        );
+        console.log(address, displayBalance);
+    }
+});
