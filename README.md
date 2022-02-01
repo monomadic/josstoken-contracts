85b15c585bf0b621024bf2077ed35fc238dccdd8
monomadic <email@server.com>
2022-02-01 16:51:55 +0800

    router task

--- README.md
+++ README.md
@@ -19,11 +19,21 @@ npx hardhat --network avax_testnet verify "0x8Fa5b4185808AeFD11b3a72C78E4b794BBb
 # mint token
 npx hardhat --network avax_testnet mintToken --token-address "0x8Fa5b4185808AeFD11b3a72C78E4b794BBb7a025" --recipient-address "0xe729259c7C6A84c3994FB33f1F32cA9081be2e80" --amount 300000
 
+# mint WAVAX
+npx hardhat --network avax_testnet deployToken --token-name "Wrapped AVAX" --token-symbol "WAVAX"
+
 # deploy factory
 npx hardhat --network avax_testnet deployFactory --fee-to-setter "0xe729259c7C6A84c3994FB33f1F32cA9081be2e80"
+npx hardhat --network avax_testnet verify "0x62a52483EB1DcB706054Ae6164f1221abE9f5aa8" "0xe729259c7C6A84c3994FB33f1F32cA9081be2e80"
+
+# deploy router
+npx hardhat --network avax_testnet deployRouter \
+    --factory-address "0x62a52483EB1DcB706054Ae6164f1221abE9f5aa8" \
+    --weth-address "0x1C226af1aB4b29e53BfDA6ffF34A1894BAeF6c9F"
 ```
 
 - deploy account: `0xe729259c7C6A84c3994FB33f1F32cA9081be2e80`
-- avax token: `0x8Fa5b4185808AeFD11b3a72C78E4b794BBb7a025`
-    - https://testnet.snowtrace.io/contract/0x8Fa5b4185808AeFD11b3a72C78E4b794BBb7a025
+- JOSS token: `0x8Fa5b4185808AeFD11b3a72C78E4b794BBb7a025`
+    - https://testnet.snowtrace.io/address/0x8Fa5b4185808AeFD11b3a72C78E4b794BBb7a025
+- WAVAX token: `0x1C226af1aB4b29e53BfDA6ffF34A1894BAeF6c9F`
 - UniswapV2Factory: `0x62a52483EB1DcB706054Ae6164f1221abE9f5aa8`
--- tasks/dexTasks.ts
+++ tasks/dexTasks.ts
@@ -3,11 +3,36 @@ import { task } from 'hardhat/config'
 task('deployFactory', 'Deploys a UniswapV2Factory')
   .addParam('feeToSetter', 'account permitted to alter feeTo')
   .setAction(async (args, hre) => {
-    const UniswapV2Factory = await hre.ethers.getContractFactory('UniswapV2Factory');
-    const uniswapV2Factory = await UniswapV2Factory.deploy(
-      args.feeToSetter,
-    );
-    const deployed = await uniswapV2Factory.deployed();
+    const UniswapV2Factory = await hre.ethers.getContractFactory(
+      'UniswapV2Factory',
+    )
+    const uniswapV2Factory = await UniswapV2Factory.deploy(args.feeToSetter)
+    const deployed = await uniswapV2Factory.deployed()
 
-    console.log(`Deployed UniswapV2Factory token to ${deployed.address}`);
+    console.log(`Deployed UniswapV2Factory token to ${deployed.address}`)
   })
+
+task('deployRouter', 'Deploys a UniswapV2Router02')
+  .addParam('factoryAddress', 'address of underlying factory')
+  .addParam('wethAddress', 'address of wrapped base token (eg. WETH, WAVAX)')
+  .setAction(async (args, hre) => {
+    const UniswapV2Router02 = await hre.ethers.getContractFactory(
+      'UniswapV2Router02',
+    )
+    const uniswapV2Router02 = await UniswapV2Router02.deploy(args.factoryAddress, args.wethAddress)
+    const deployed = await uniswapV2Router02.deployed()
+
+    console.log(`Deployed UniswapV2Router02 to ${deployed.address}`)
+  })
+
+// task('createPair', 'Create a UniswapV2 Pair')
+//   .addParam('feeToSetter', 'account permitted to alter feeTo')
+//   .setAction(async (args, hre) => {
+//     const UniswapV2Factory = await hre.ethers.getContractFactory('UniswapV2Factory');
+//     const uniswapV2Factory = await UniswapV2Factory.deploy(
+//       args.feeToSetter,
+//     );
+//     const deployed = await uniswapV2Factory.deployed();
+
+//     console.log(`Deployed UniswapV2Factory token to ${deployed.address}`);
+//   })
